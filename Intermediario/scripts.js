a8b303d38d343414c68c6716884517eef5c86094
Helio Franca <helio.franca@rsinet.com.br>
2019-06-18 18:26:43 -0300

    Exemplos modulo avançado

--- Intermediario/scripts.js
+++ Intermediario/scripts.js
@@ -4,8 +4,17 @@ function Pencil(){
 	this.length=15;
 	this.write = function(text){
 		console.log(text);
+		debugger;
 	}
+}
+
+function esconder(){
+		var campo = document.querySelector("fieldset");
+		campo.hidden=true
 }
 
 var myPencil = new Pencil();
 myPencil instanceof  Pencil
+
+
+
